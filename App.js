4e7ae0ddb9bf0dc082ab9a4f55929e8b28998fbe
Tetsu9901 <t.marion@lyon.ort.asso.fr>
2023-04-19 16:24:57 +0200

    Step 1

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.red}>Open up <Text style={styles.bold}>App.js</Text> to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +17,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  red:{
+    color:'red',
+  },
+  bold:{
+    fontWeight:'bold',
+  }
 });
