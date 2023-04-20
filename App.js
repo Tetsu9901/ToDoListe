16a411323840e9112da04b86f00540d41c855594
Tetsu9901 <t.marion@lyon.ort.asso.fr>
2023-04-20 13:50:53 +0200

    Refacto Step 2

--- App.js
+++ App.js
@@ -1,14 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { Button, FlatList, StyleSheet, Text, TextInput, View} from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  Button,
+  FlatList,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.red}>Open up <Text style={styles.bold}>App.js</Text> to start working on your app!</Text>
-      <FlatList data={sampleGoals} renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}/>
+      <Text style={styles.red}>
+        Open up <Text style={styles.bold}>App.js</Text> to start working on your
+        app!
+      </Text>
+      <View>
+        {sampleGoals.map((sampleGoals) => {
+          return (
+            <View>
+              <Text style={styles.item}>{sampleGoals}</Text>
+            </View>
+          );
+        })}
+      </View>
       <View style={styles.raw}>
-      <TextInput placeholder="Type here to translate!"></TextInput>
-      <Button title="Add"></Button>
+        <TextInput placeholder="Type here to translate!"></TextInput>
+        <Button title="Add"></Button>
       </View>
       <StatusBar style="auto" />
     </View>
@@ -17,20 +35,20 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingTop: 100,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingTop: 75,
   },
-  red:{
-    color:'red',
+  red: {
+    color: "red",
     fontSize: 18,
   },
-  bold:{
-    fontWeight:'bold',
+  bold: {
+    fontWeight: "bold",
   },
-  raw:{
-    flexDirection: 'row',
+  raw: {
+    flexDirection: "row",
   },
   item: {
     padding: 10,
@@ -40,14 +58,14 @@ const styles = StyleSheet.create({
 });
 
 const sampleGoals = [
-  {key: "Faire les courses"},
-  {key: "Aller à la salle de sport 3 fois par semaine"},
-  {key: "Montaer à plus de 5000m d'altitude"},
-  {key: "Acheter mon premier appartement"},
-  {key: "Perdre 5kgs"},
-  {key: "Gagner en productivité"},
-  {key: "Apprendre un nouveau langage"},
-  {key: "Faire une mission en freelance"},
-  {key: "Organiser un meetup autour de la tech"},
-  {key: "Faire un triathlon"}
+  "Faire les courses",
+  "Aller à la salle de sport 3 fois par semaine",
+  "Montaer à plus de 5000m d'altitude",
+  "Acheter mon premier appartement",
+  "Perdre 5kgs",
+  "Gagner en productivité",
+  "Apprendre un nouveau langage",
+  "Faire une mission en freelance",
+  "Organiser un meetup autour de la tech",
+  "Faire un triathlon",
 ];
