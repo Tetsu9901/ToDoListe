a5c87f78fba286dcb86d67c8aff8f6e36a8b5be6
Tetsu9901 <t.marion@lyon.ort.asso.fr>
2023-05-15 17:05:58 +0200

    Step 3

--- App.js
+++ App.js
@@ -1,15 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
-import { Button, FlatList, StyleSheet, Text, TextInput, View} from 'react-native';
+import React, { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import {
+  Button,
+  FlatList,
+  Pressable,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  Modal,
+  TouchableOpacity,
+} from "react-native";
 
 export default function App() {
+  const [text, setText] = useState("");
+  const [sampleGoals, setGoals] = useState([
+    { key: "Faire les courses" },
+    { key: "Aller à la salle de sport 3 fois par semaine" },
+    { key: "Monter à plus de 5000m d'altitude" },
+    { key: "Acheter mon premier appartement" },
+    { key: "Perdre 5kgs" },
+    { key: "Gagner en productivité" },
+    { key: "Apprendre un nouveau langage" },
+    { key: "Faire une mission en freelance" },
+    { key: "Organiser un meetup autour de la tech" },
+    { key: "Faire un triathlon" },
+  ]);
+  const addGoal = () => {
+    text && setGoals([...sampleGoals, { key: text }]);
+  };
+  deleteGoal = (key) => {
+    setGoals(sampleGoals => sampleGoals.filter((item, i) => item.key !== key));
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.red}>Open up <Text style={styles.bold}>App.js</Text> to start working on your app!</Text>
-      <FlatList data={sampleGoals} renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}/>
-      <View style={styles.raw}>
-      <TextInput placeholder="Type here to translate!"></TextInput>
-      <Button title="Add"></Button>
-      </View>
+      <Text style={styles.red}>
+        Open up <Text style={styles.bold}>App.js</Text> to start working on your
+        app!
+      </Text>
+    <View style={styles.row}>
+      <TextInput
+        placeholder="Type here to add a line"
+        onChangeText={(text) => setText(text)}
+        value={text}
+      ></TextInput>
+      <Pressable onPress={addGoal}>
+        <Text style={styles.buttonAdd}>Add</Text>
+      </Pressable>
+    </View>
+      <FlatList
+        data={sampleGoals}
+        renderItem={({ item }) => (
+          <View style={styles.item}>
+            <Text>{item.key}</Text>
+            <TouchableOpacity onPress={()=>this.deleteGoal(item.key)} style={styles.buttonDelete}>
+              <Text style={styles.textBtnDel}>X</Text>
+            </TouchableOpacity>
+          </View>
+        )}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -17,37 +67,50 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingTop: 100,
+    flex: 0,
+    backgroundColor: "#D9F0FF",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingTop: 60,
   },
-  red:{
-    color:'red',
+  red: {
+    color: "red",
+    fontSize: 18,
+    marginBottom: 20
+  },
+  buttonAdd: {
+    padding: 10,
+    color: "blue",
     fontSize: 18,
   },
-  bold:{
-    fontWeight:'bold',
+  blue: {
+    color: "blue",
+    fontSize: 18,
   },
-  raw:{
-    flexDirection: 'row',
+  bold: {
+    fontWeight: "bold",
+  },
+  row: {
+    flexDirection: "row",
   },
   item: {
-    padding: 10,
-    fontSize: 15,
-    height: 30,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    backgroundColor: "white",
+    marginBottom: 15,
+    padding: 9,
+    borderRadius: 6,
   },
+  buttonDelete: {
+    alignItems: "center",
+    backgroundColor: "#6F73D2",
+    marginBottom: 1,
+    marginLeft: 25,
+    padding: 9,
+    borderRadius: 6,
+  },
+  textBtnDel: {
+    color: 'white',
+  }
 });
-
-const sampleGoals = [
-  {key: "Faire les courses"},
-  {key: "Aller à la salle de sport 3 fois par semaine"},
-  {key: "Montaer à plus de 5000m d'altitude"},
-  {key: "Acheter mon premier appartement"},
-  {key: "Perdre 5kgs"},
-  {key: "Gagner en productivité"},
-  {key: "Apprendre un nouveau langage"},
-  {key: "Faire une mission en freelance"},
-  {key: "Organiser un meetup autour de la tech"},
-  {key: "Faire un triathlon"}
-];
